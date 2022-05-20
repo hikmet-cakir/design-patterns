6d75794297833880362680867ce62918b50ea8ef
Hikmet <hikmet_cakir@outlook.com>
2022-05-21 00:49:24 +0300

    Edited Lazy Instantiation Singleton Pattern Structure

--- singleton/lazyinstantiation/threadsafe/Earth.java
+++ singleton/lazyinstantiation/threadsafe/Earth.java
@@ -13,24 +13,24 @@ public class Earth {
     // This method is used for get to Earth
     public static Earth getInstance() {
         if(instance == null) {
-            synchronized(Earth.class) {
-	if(instance == null) {
-                     instance = new Earth();
-	}
-            } 
+            synchronized (Earth.class) {
+                if(instance == null) {
+                    instance = new Earth();
+                }
+            }
         }
         return instance;
     }
     // If you want to increase population, you have to use this method
-    public void addToPopulation(int addAmount) {
+    public synchronized void addToPopulation(int addAmount) {
         population += addAmount;
     }
     // If you want to decrease population, you have to use this method
-    public void subtractFromPopulation(int subtractAmount) {
+    public synchronized void subtractFromPopulation(int subtractAmount) {
         population -= subtractAmount;
     }
     // If you want to get to population, you have to use this method
-    public int getPopulation() {
+    public synchronized int getPopulation() {
         return population;
     }
 }
