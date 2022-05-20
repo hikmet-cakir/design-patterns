7361e29637f74decc117482b5ca47908e023db14
Surabaya <hikmetcakiir@outlook.com>
2022-05-20 20:37:02 +0300

    Added Lazy Instantiation Singleton Design Pattern

--- singleton/lazyinstantiation/Earth.java
+++ singleton/lazyinstantiation/Earth.java
@@ -6,12 +6,15 @@ public class Earth {
     // It's used for indicate to Earth's living population
     private int population = 0;
     // It indicate Earth our used
-    private static final Earth instance = new Earth();
+    private static Earth instance;
     // We don't want to multiple instance so we have to set access modifier to private
     private Earth() {
     }
     // This method is used for get to Earth
     public static Earth getInstance() {
+        if(instance == null) {
+            instance = new Earth(); // Don't forget this situation is not THREAD SAFE!
+        }
         return instance;
     }
     // If you want to increase population, you have to use this method
