59099234fe8f7a958c513973f09c457e09d3ca70
Hikmet Çakır <hikmet_cakir@outlook.com>
2022-07-18 14:10:49 +0300

    Added Observer Design Pattern

---
+++ observer/Salesman.java
@@ -0,0 +1,23 @@
+import java.io.File;
+
+public class Salesman {
+    public EventManager events;
+    private File file;
+
+    public Salesman() {
+        this.events = new EventManager("open", "save");
+    }
+
+    public void openFile(String filePath) {
+        this.file = new File(filePath);
+        events.notify("open", file);
+    }
+
+    public void saveFile() throws Exception {
+        if (this.file != null) {
+            events.notify("save", file);
+        } else {
+            throw new Exception("Please open a file first.");
+        }
+    }
+}
